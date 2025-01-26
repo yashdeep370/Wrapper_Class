02819fbdd45f5c600ebdcea2ad9329578793835a
Yash Deep <143354431+yashdeep370@users.noreply.github.com>
2025-01-26 20:26:24 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,26 @@
+public class wrapper {
+        public static void main(String[] args) {
+            Integer i = new Integer(10);
+            Integer a = Integer.valueOf(10);
+            Integer b = 10;
+
+            Byte c = 15;
+            Byte d = Byte.valueOf("15");
+
+            byte bb=15;
+            Byte e = Byte.valueOf(bb);
+
+            Short f = Short.valueOf("123");
+
+            Float g = 12.3f;
+
+            Float h = Float.valueOf("123.5");
+            
+            Double j = Double.valueOf(123.456);
+
+            Character k= Character.valueOf('A');
+
+            Boolean l = Boolean.valueOf("true");
+
+        }
+}
